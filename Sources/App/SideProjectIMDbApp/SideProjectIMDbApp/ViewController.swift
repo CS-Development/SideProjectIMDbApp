1caf05ae69d6bf8ebbadb7f532462bfb17e0f53e
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-24 15:12:40 +0200

    create app project

---
+++ Sources/App/SideProjectIMDbApp/SideProjectIMDbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideProjectIMDbApp
+//
+//  Created by Christian Slanzi on 24.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
