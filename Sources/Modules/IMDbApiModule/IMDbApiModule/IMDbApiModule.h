7ca564c5ec5ac96cd4118774fb072ad10831444e
Christian Slanzi <christian.slanzi@gmail.com>
2021-07-26 20:13:09 +0200

    created IMDbApiModule and moved our IMDbApiService and manager there

---
+++ Sources/Modules/IMDbApiModule/IMDbApiModule/IMDbApiModule.h
@@ -0,0 +1,18 @@
+//
+//  IMDbApiModule.h
+//  IMDbApiModule
+//
+//  Created by Christian Slanzi on 26.07.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IMDbApiModule.
+FOUNDATION_EXPORT double IMDbApiModuleVersionNumber;
+
+//! Project version string for IMDbApiModule.
+FOUNDATION_EXPORT const unsigned char IMDbApiModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMDbApiModule/PublicHeader.h>
+
+
