6509bd2bfa005306194d05e9d7acd17cf1d64f7d
Christian Slanzi <christian.slanzi@gmail.com>
2021-08-16 20:07:19 +0200

    add tab bar controller with three tabs

--- Sources/App/SideProjectIMDbApp/SideProjectIMDbApp/Main/IMDbAppDependencies.swift
+++ Sources/App/SideProjectIMDbApp/SideProjectIMDbApp/Main/IMDbAppDependencies.swift
@@ -19,6 +19,19 @@ class IMDbAppDependencies {
     
     static let shared = IMDbAppDependencies()
     
+    private lazy var client: HTTPClient = {
+        return URLSessionHTTPClient(session: URLSession.shared)
+    }()
+    
+    private lazy var service: IMDbApiServiceProtocol = {
+        let apiKey = "k_4olf5ls3"
+        return IMDbApiService(baseURL: URL(string: "https://imdb-api.com")!, client: client, apiKey: apiKey)
+    }()
+    
+    private lazy var imdbManager: IMDbManagerProtocol = {
+        return IMDbManager(service: service)
+    }()
+    
     public func setScene(_ scene: UIScene) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
@@ -36,16 +49,34 @@ class IMDbAppDependencies {
         let isLoggedIn:Bool = true
         
         if isLoggedIn {
-            let apiKey = "k_4olf5ls3"
-            let client = URLSessionHTTPClient(session: URLSession.shared)
-            let service: IMDbApiServiceProtocol  = IMDbApiService(baseURL: URL(string: "https://imdb-api.com")!, client: client, apiKey: apiKey)
-            let manager: IMDbManagerProtocol = IMDbManager(service: service)
-            
-            setRootViewController(HomeViewController(manager: manager))
+            setRootViewController(makeMainTabBarController(manager: self.imdbManager))
         } else {
             setRootViewController(LoginViewController())
         }
 
+    }
+    
+    private func makeHomeViewController(manager: IMDbManagerProtocol) -> UIViewController {
+        return HomeViewController(manager: manager)
+    }
+    
+    private func makeMainTabBarController(manager: IMDbManagerProtocol) -> UIViewController {
+        
+        let homeVC = makeHomeViewController(manager: manager)
+        let searchVC = makeSearchViewController(manager: manager)
+        let profileVC = makeProfileViewController(manager: manager)
+        
+        let tabController = MainTabBarController(
+            viewControllers: [homeVC, searchVC, profileVC])
+        return tabController
+    }
+    
+    private func makeSearchViewController(manager: IMDbManagerProtocol) -> UIViewController {
+        return SearchViewController(manager: manager)
+    }
+    
+    private func makeProfileViewController(manager: IMDbManagerProtocol) -> UIViewController {
+        return ProfileViewController(manager: manager)
     }
 }
  
@@ -55,5 +86,45 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .cyan
+    }
+}
+
+class SearchViewController: UIViewController {
+    
+    private let manager: IMDbManagerProtocol
+    
+    init(manager: IMDbManagerProtocol) {
+        self.manager = manager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
+
+class ProfileViewController: UIViewController {
+    
+    private let manager: IMDbManagerProtocol
+    
+    init(manager: IMDbManagerProtocol) {
+        self.manager = manager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
     }
 }
